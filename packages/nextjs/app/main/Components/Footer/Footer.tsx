06e64939d3ec3bf2e4a3310ba7b40ce1e3dc357c
andrecamerino <camerinoandre@gmail.com>
2025-03-22 05:13:08 +1300

    create footer

---
+++ packages/nextjs/app/main/Components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+import Button from '../Button/Button';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <Button label="Play!" href="https://example.com" />
+      <Button label="Play!" href="https://example.com" />
+      <Button label="Play!" href="https://example.com" />
+      <Button label="Play!" href="https://example.com" />
+      <Button label="Play!" href="https://example.com" />
+      <Button label="Play!" href="https://example.com" />
+      <Button label="Play!" href="https://example.com" />
+    </div>
+  );
+}
+
+export default Footer;
