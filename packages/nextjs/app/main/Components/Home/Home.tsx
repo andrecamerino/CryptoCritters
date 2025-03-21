fd46bd32581d1cbbcd6aeffd63e6b13547ff49a3
andrecamerino <camerinoandre@gmail.com>
2025-03-22 07:03:52 +1300

    change home background

--- packages/nextjs/app/main/Components/Home/Home.tsx
+++ packages/nextjs/app/main/Components/Home/Home.tsx
@@ -1,22 +1,26 @@
-// Home.tsx (or whatever your main page component is)
-'use client'; // Add this line to make the component a client-side component
+"use client";
 
-import React from 'react';
-import Button from '../Button/Button'; // Assuming Button is the component you need
-import Island from '../Island/Island';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import React from "react";
+import Button from "../Button/Button";
+import Cloud from "../Cloud/Cloud";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Animal from "../Animal/Animal";
+import "./Home.css";
 
 const Home: React.FC = () => {
   const handleClick = () => {
-    alert('Button clicked!');
+    alert("Button clicked!");
   };
 
   return (
-    <div>
+    <div className="home">
       <Header />
-        <Island />
-        <Footer />
+      <Cloud height="-120px" speed="300s" direction="reverse" size={180} />
+      <Cloud height="-120px" speed="180s" direction="normal" size={240} />
+      <Cloud height="-120px" speed="100s" direction="reverse" size={400} />
+      <Animal />
+      <Footer />
     </div>
   );
 };
