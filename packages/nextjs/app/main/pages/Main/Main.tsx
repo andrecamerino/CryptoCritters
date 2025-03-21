77657fa882e9b09f430ca02c482647e164430f20
andrecamerino <camerinoandre@gmail.com>
2025-03-22 05:13:28 +1300

    create main + bug fix

---
+++ packages/nextjs/app/main/pages/Main/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Main.css'
+import Header from '../../Components/Header/Header'
+import Home from '../../Components/Home/Home'
+import Footer from '../../Components/Footer/Footer'
+
+const home = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default home
