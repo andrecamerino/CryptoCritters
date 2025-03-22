b6228930930a63d35eab4d969e6e1b59c9675a28
andrecamerino <camerinoandre@gmail.com>
2025-03-22 20:20:40 +1300

    edit footer

--- packages/nextjs/Components/Footer/Footer.tsx
+++ packages/nextjs/Components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <Button label="Play!" href="/EggStore/EggStore" />
+      <Button label="Egg Store" href="/EggStore/EggStore" />
       <div className="border border-black-500 mt-10">{/* <Tablet /> */}</div>
       <Button label="Play!" href="https://example.com" />
     </div>
